b5ed18086b24a490eb1de01373105f611b43e282
jrocco2 <34151148+jrocco2@users.noreply.github.com>
2023-02-09 11:07:48 +1100

    Initial Website and contracts are up

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: "6DKHW4EGTT88DHFGHU139CH5FRVK9PF3ZW", // Ethereum
+  },
+};
+
+export default config;
